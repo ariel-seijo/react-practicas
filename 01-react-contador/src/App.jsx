82f07acd70e40f9f6eca0b53c309c4d457b1a78f
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-05 14:42:53 -0300

    Se resuelve el ejercicio 01-react-contador

--- 01-react-contador/src/App.jsx
+++ 01-react-contador/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+export default App;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contador, setContador] = useState(0);
+
+  const sumar = () => {
+    setContador((prev) => prev + 1);
+  };
+  const restar = () => {
+    if (contador === 0) {
+      alert("El número no puede ser menor a 0.");
+      return;
+    }
+    setContador((prev) => prev - 1);
+  };
+
+  const reiniciar = () => {
+    setContador(0);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="contador">{contador}</h1>
+      <button className="sumar" onClick={sumar}>
+        sumar
+      </button>
+      <button className="restar" onClick={restar}>
+        restar
+      </button>
+      <button className="reiniciar" onClick={reiniciar}>
+        reiniciar
+      </button>
     </>
-  )
+  );
 }
-
-export default App
