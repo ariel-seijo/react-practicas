f183609e61aa3bf6bfa568e5f76e7c0ef7263491
Ariel <hariel.seijo@gmail.com>
2026-03-07 23:42:33 -0300

    se resuelve el ejercicio 06-react-page-title

--- 06-react-page-title/src/App.jsx
+++ 06-react-page-title/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+export default App
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    setCount((prev) => prev + 1);
+  }
+  
+  useEffect(() => {
+    document.title = `Contador: ${count}`}, 
+    [count]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>CONTADOR: </h1>
+      <button onClick={addCount}>Aumentar contador</button>
+      <h2>{count}</h2>
     </>
   )
 }
 
-export default App
