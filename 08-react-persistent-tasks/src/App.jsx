a1438cbbdf922ce503c52ad157f8990695aa38de
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-09 00:53:11 -0300

    se resuelve el ejercicio 08-react-persistent-list

--- 08-react-persistent-tasks/src/App.jsx
+++ 08-react-persistent-tasks/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [list, setList] = useState(() => {
+    const saved = localStorage.getItem("tasks");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const enterTask = (e) => {
+    setTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (!task.trim()) return;
+    setList((prev) => [...prev, task]);
+    setTask("");
+  };
+
+  const deleteTask = (indexToDelete) => {
+    setList((prev) => prev.filter((_, index) => index !== indexToDelete));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(list));
+  }, [list]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>LISTA DE TAREAS</h1>
+      <input value={task} onChange={enterTask}></input>
+      <button onClick={addTask}>Agregar tarea</button>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteTask(index)}>X</button>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
