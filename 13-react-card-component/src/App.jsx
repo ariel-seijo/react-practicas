8a5b58a813e734f4e96ce69d4a3ea8b2c199cf5c
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-10 17:58:41 -0300

    se realiza el ejercicio 13-react-card-component

--- 13-react-card-component/src/App.jsx
+++ 13-react-card-component/src/App.jsx
@@ -1,5 +1,14 @@
 import "./App.css";
+import Card from "./components/Card";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <Card title="Naruto" description="anime sobre ninjas" />
+      <Card title="Bleach" description="anime sobre shinigamis" />
+      <Card title="Dragon Ball" description="anime sobre marcianos" />
+    </>
+  );
+}
 
 export default App;
