9bb0b6632514c585a4ac9739886ecb9679a2b723
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-10 18:32:55 -0300

    se realiza el ejercicio 14-react-layout-components

---
+++ 14-react-layout-components/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+function Main() {
+  return (
+    <main>
+      <Card title="Naruto" description="anime sobre ninjas" />
+      <Card title="Bleach" description="anime sobre shinigamis" />
+      <Card title="Dragon Ball" description="anime sobre marcianos" />
+    </main>
+  );
+}
+
+export default Main;
