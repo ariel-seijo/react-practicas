3d8891683f57fbba3d238c80ebffaa51fe86ce1f
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-11 23:03:42 -0300

    Se realiza el ejercicio 17-react-tabs

--- 17-react-tabs/src/App.jsx
+++ 17-react-tabs/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeTab, setActiveTab] = useState("profile");
 
   return (
     <>
+      <h1>Tabs</h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setActiveTab("profile")}>Perfil</button>
+
+        <button onClick={() => setActiveTab("settings")}>Configuración</button>
+
+        <button onClick={() => setActiveTab("security")}>Seguridad</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        {activeTab === "profile" && <p>Contenido del perfil</p>}
+        {activeTab === "settings" && <p>Contenido de configuración</p>}
+        {activeTab === "security" && <p>Contenido de seguridad</p>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
