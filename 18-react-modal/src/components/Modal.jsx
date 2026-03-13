e54fb6de30ee4f0c1b3b4e340391068687b962a8
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-12 22:21:07 -0300

    se agregan mejoras: 1. cerrar modal al presionar el fondo, pero no al presionar el modal. 2. cerrar modal al presionar ESC

--- 18-react-modal/src/components/Modal.jsx
+++ 18-react-modal/src/components/Modal.jsx
@@ -1,9 +1,21 @@
 import "./Modal.css";
+import { useEffect } from "react";
 
 function Modal({ onClose }) {
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
-    <div className="overlay">
-      <div className="modal">
+    <div className="overlay" onClick={onClose}>
+      <div className="modal" onClick={(event) => event.stopPropagation()}>
         <h2>Esto es un modal</h2>
         <button onClick={onClose}>Cerrar</button>
       </div>
