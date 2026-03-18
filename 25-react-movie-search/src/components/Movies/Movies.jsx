8bbfadc522ae12231fddac0eae509a2859fbc7d9
Ariel Seijo <hariel.seijo@gmail.com>
2026-03-17 23:07:36 -0300

    agrego input para busqueda

--- 25-react-movie-search/src/components/Movies/Movies.jsx
+++ 25-react-movie-search/src/components/Movies/Movies.jsx
@@ -2,11 +2,21 @@ import { useState, useEffect } from "react";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
-    fetch("https://www.omdbapi.com/?apikey=85ececc1&s=basdasadsdasan")
+    if (!search) {
+      return;
+    }
+    setLoading(true);
+    fetch(`https://www.omdbapi.com/?apikey=85ececc1&s=${search}`)
       .then((response) => response.json())
       .then((data) => {
         if (data.Response === "False") {
@@ -21,13 +31,18 @@ function Movies() {
         setError("Error al cargar las peliculas");
         setLoading(false);
       });
-  }, []);
+  }, [search]);
 
   if (loading) return <h2>Cargando peliculas...</h2>;
   if (error) return <h2>{error}</h2>;
 
   return (
     <>
+      <input
+        value={search}
+        placeholder="batman, avengers, etc"
+        onChange={handleSearch}
+      ></input>
       <ul>
         {movies.map((movie) => (
           <li key={movie.imdbID}>
